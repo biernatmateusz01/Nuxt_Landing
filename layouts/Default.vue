818d1cd633f4d0ca4fee6412087ca09c46824019
biernatmateusz01 <biernat521@gmai.com>
2023-03-30 21:19:05 +0200

    strona w budowie

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="bg-[#282162] min-h-screen">
-    <TheHeader />
-    <slot />
+    <!-- <TheHeader /> -->
+    <!-- <slot /> -->
     <!-- <TheFooter /> -->
+    <div class="fixed top-0left-0 h-screen w-screen flex items-center justify-center">
+      <span class="text-4xl text-center text-white font-semibold">Strona w budowie! <br> Zapraszamy za kilka dni</span>
+    </div>
   </div>
 </template>
